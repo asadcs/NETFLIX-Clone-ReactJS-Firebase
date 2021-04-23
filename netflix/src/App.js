dd1e38241597fc2f3ef0859bd3dd0a71cb154ced
Muhammad Asadullah <muhammad.asad2020@gmail.com>
2021-04-23 14:24:58 -0700

    Fetch API implemented to get movies data

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
+import requests from './requests';
 import './App.css';
+import Row from './Row';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Row title="Netflix" fetchUrl={requests.fetchTrending}></Row>
+     </div>
   );
 }
 
