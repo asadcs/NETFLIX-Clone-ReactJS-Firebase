25d2054c3737264d6a34ee95eaf0328de55c6fc9
Muhammad Asadullah <muhammad.asad2020@gmail.com>
2021-04-24 03:38:53 -0700

    Add Trailer popup done

--- netflix/src/Row.js
+++ netflix/src/Row.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "./axios";
 import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLarge }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -18,7 +21,31 @@ function Row({ title, fetchUrl, isLarge }) {
     fetchData();
   }, [fetchUrl]);
 
-  console.table(movies);
+  //console.table(movies);
+  const youtubeOpts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+  const movieClickedHandler = (movie) => {
+    console.log(movie?.name || movie?.title || movie?.orginal_name || "");
+
+    if (trailerUrl != "") setTrailerUrl("");
+    else {
+      movieTrailer(movie?.name || movie?.title || movie?.orginal_name || "")
+        .then((url) => {
+          console.log(url);
+          const urlParamV = new URLSearchParams(new URL(url).search);
+          console.log(urlParamV.get("v"));
+          setTrailerUrl(urlParamV.get("v"));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
 
   return (
     <div className="row">
@@ -26,6 +53,7 @@ function Row({ title, fetchUrl, isLarge }) {
       <div className="row__posters">
         {movies.map((movie) => (
           <img
+            onClick={() => movieClickedHandler(movie)}
             key={movie.id}
             src={`${base_url}${
               isLarge ? movie.poster_path : movie.backdrop_path
@@ -34,6 +62,7 @@ function Row({ title, fetchUrl, isLarge }) {
           />
         ))}
       </div>
+      {trailerUrl != "" && <YouTube videoId={trailerUrl} opts={youtubeOpts} />}
     </div>
   );
 }
